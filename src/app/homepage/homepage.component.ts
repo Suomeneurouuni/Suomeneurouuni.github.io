07656713e9276e46e13f35fe504fbee86699086e
Suomeneurouuni <104135720+Suomeneurouuni@users.noreply.github.com>
2022-04-28 09:54:34 +0300

    Update homepage.component.ts

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,8 @@ import { TekstiStorageService } from '../shared/teksti-storage.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -12,16 +14,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomepageComponent implements OnInit {
 
+  display = false;
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+
   constructor(private tekstiStorageService: TekstiStorageService,
-             
+         private authService: AuthService,
               public router: Router,) { }
 
   ngOnInit() {
- 
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
       this.tekstiStorageService.fetchTekstit().subscribe(); 
 
   
  
 
 }
-}
+
+onPress() {
+  this.display = ! this.display;
+  if (this.display = this.display) {
+    this.router.navigate(['/auth']);
+  
+  } else {
+    this.router.navigate(['../'])
+  }
+}}
